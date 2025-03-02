abfe999ed879a8a836defc03cc26b5e98da1013a
RhythmChandak <rhythm@seas.upenn.edu>
2025-03-02 18:45:31 -0500

    event_cam_subscriber node written to get events from topic

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-REPO FOR RUNNING METAVISION DRIVER
+# REPO FOR RUNNING METAVISION DRIVER
+
+## Steps to Launch
+
+#### Terminal 1:
+
+```bash
+metavision_driver_ws$ ros2 launch metavision_driver driver_node.launch.py
+```
+
+#### Terminal 2:
+
+```bash
+metavision_driver_ws$ ros2 launch event_camera_renderer renderer.launch.py
+```
+
+#### Terminal 3:
+
+To visualize the data from event camera
+```bash
+metavision_driver_ws$ ros2 run rqt_image_view rqt_image_view
+```
+
+#### Terminal 4:
+To subscribe to the data from event camera 
+```bash
+metavision_driver_ws$ ros2 run event_cam_subscriber event_cam_subscriber 
+```
---
+++ src/event_cam_subscriber/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'event_cam_subscriber'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='pi',
+    maintainer_email='rhythm@seas.upenn.edu',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'event_cam_subscriber = event_cam_subscriber.subscriber_node:main',
+        ],
+    },
+)
